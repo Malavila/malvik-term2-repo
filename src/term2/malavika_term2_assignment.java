9040c01b9a6742565fe7470e5a3574f4b035d5d1
Malavika K <malu4353@gmail.com>
2021-09-04 16:28:52 +0530

    1st checkin

---
+++ src/term2/malavika_term2_assignment.java
@@ -0,0 +1,10 @@
+package term2;
+
+public class malavika_term2_assignment {
+	
+	public static void main(String[] args) {
+        System.out.println("Welcome Puneet to the Term 2 Assessment");
+	}
+
+}
+

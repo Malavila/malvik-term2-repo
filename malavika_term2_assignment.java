06f62af7d0dae98ad356ec673889a1972b5f886e
Malavika <88318199+Malavila@users.noreply.github.com>
2021-09-04 16:59:00 +0530

    Add files via upload

---
+++ malavika_term2_assignment.java
@@ -0,0 +1,9 @@
+package term2;
+
+public class malavika_term2_assignment {
+
+	public static void main(String[] args) {
+        System.out.println("Welcome malavika to the Term 2 Assessment");
+	}
+
+}
